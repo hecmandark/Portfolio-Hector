60b60959d04d859972a878d81a24ccd96e47778c
hecmandark <{email}>
2025-05-14 16:48:49 +0200

    Update settings: allow all hosts in development environment

--- portfolio_blog/portfolio_blog/settings.py
+++ portfolio_blog/portfolio_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&gjqhgm7i4a#%gad+&+utwf16f^!p1l$hg9$_=0oj77u!o1v#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
